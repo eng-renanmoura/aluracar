221c667d0edc7c849b35b6ce4060f9ec212736ff
Renan Moura <renan.ferreira@trt8.jus.br>
2018-08-03 14:46:03 -0300

    parte 1 aula 1

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,12 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  private carros;
   constructor(public navCtrl: NavController) {
-
+    this.carros=[
+      {nome:"Golf", preco:"12000"},
+      {nome:"Palio", preco:"300"}
+    ];
   }
 
 }
